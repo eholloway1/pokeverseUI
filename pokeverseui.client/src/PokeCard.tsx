4cebc4b0229b070d6952f16028231e65d7752321
Evan.Laptop <eholloway00@gmail.com>
2024-09-28 14:56:24 -0700

    adds a pokemon card component

---
+++ pokeverseui.client/src/PokeCard.tsx
@@ -0,0 +1,14 @@
+import Card from 'react-bootstrap/Card';
+
+function PokeCards() {
+    return (
+        <Card>
+            <Card.Img variant="top" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" />
+            <Card.Body>
+                text
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default PokeCards;   
