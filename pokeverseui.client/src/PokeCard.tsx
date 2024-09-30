5b1f0bf7a6dbdde4d9e26041c8b8d1d15c103ffd
Evan Holloway <evan.holloway@alaskaair.com>
2024-09-30 14:42:27 -0700

    technical diff

--- pokeverseui.client/src/PokeCard.tsx
+++ pokeverseui.client/src/PokeCard.tsx
@@ -1,14 +1,19 @@
 import Card from 'react-bootstrap/Card';
 
-function PokeCards() {
-    return (
-        <Card>
-            <Card.Img variant="top" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" />
-            <Card.Body>
-                text
-            </Card.Body>
-        </Card>
-    );
+interface CardProps {
+  PokeName: string;
+  PokeImg: string;
+}
+
+function PokeCards({ PokeName, PokeImg }: CardProps) {
+  return (
+      <Card>
+        <Card.Img variant="top" src={PokeImg} />
+        <Card.Body>
+          {PokeName}
+        </Card.Body>
+      </Card>
+  );
 }
 
 export default PokeCards;   
