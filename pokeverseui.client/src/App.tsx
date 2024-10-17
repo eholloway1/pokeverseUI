3fc109c505c14d3662f36a9a3a1787516bf443f6
Evan Holllway <Evan.Holloway@alaskaair.com@SEAHOEHOLLO.local>
2024-10-17 14:37:44 -0700

    Implements 151 card display

--- pokeverseui.client/src/App.tsx
+++ pokeverseui.client/src/App.tsx
@@ -1,41 +1,21 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import PokeCards from './PokeCard';
-import {PokeJson, pokeDetails, PokeResults, SpriteList} from './Models/PokeJson.tsx';
+import {PokeJson, PokeResults} from './Models/PokeJson.tsx';
 
 function App() {
   const url = "https://pokeapi.co/api/v2/pokemon?limit=151";
 
   const [unfilteredPoke, setUnfilteredPoke] = useState<PokeJson[] | undefined>([]);
   const [jsonResult, setJsonResult] = useState<PokeResults>();
-  const [pokeDetails, setPokeDetails] = useState<pokeDetails>();
-  const serialSprites:(SpriteList | undefined)[] = [];
-  const pokeSprites:(string | undefined)[] = [];
+
   useEffect(() => {
-    const fetcher = async () => {
-      const response = await fetch(url);
-      const json = await response.json(); //This returns and object with count, next, prev, results. The results contain an array of objects holding the name and url to links of details for X number of pokemon requested
-      await setJsonResult(json); // deserialize the json
-      await setUnfilteredPoke(jsonResult?.results); // deserialize into an array of object containing the name and details url
-      await unfilteredPoke?.map(async (poke) => {
-        const response = await fetch(poke.url);
-        const json = await response.json();
-        setPokeDetails(json);
-        serialSprites.push(pokeDetails?.sprites);
-        // pokeSprites.push(pokeDetails?.sprites.front_default);
-      }); // fetch the objects returned from the details url deserialize the data into an array of object containing the shiny sprite
-      serialSprites.map((p => {pokeSprites.push(p?.front_default)}));
-
-    };
-
-    fetcher();
-  }, [serialSprites, pokeSprites]);
+    fetch(url).then(response => response.json()).then(data => setJsonResult(data)).finally(() => setUnfilteredPoke(jsonResult?.results));
+  }, [unfilteredPoke]);
 
   return (
-
-    <div>
-      <h1>{serialSprites[0]?.front_default}</h1>
-      {unfilteredPoke?.map((poke, index) => <PokeCards PokeName={poke.name} PokeImg={serialSprites[index]?.front_default} /> )}
+    <div className="flexContainer">
+      {unfilteredPoke?.map((poke) =>  <PokeCards PokeObj={poke} />)}
     </div>
   );
 }
