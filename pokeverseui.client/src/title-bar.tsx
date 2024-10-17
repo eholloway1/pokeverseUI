bc0211a8a63bd976443f56b36c670cb695c58723
Evan Holllway <Evan.Holloway@alaskaair.com@SEAHOEHOLLO.local>
2024-10-17 14:58:04 -0700

    Adds page title

---
+++ pokeverseui.client/src/title-bar.tsx
@@ -0,0 +1,12 @@
+import './title-bar.css'
+function TitleBar(){
+    return(
+        <div className='TitleCard'>
+            <h2>
+                &nbsp;&nbsp;&nbsp;&nbsp;PokeVerse&nbsp;|&nbsp;&nbsp;All Pokemon
+            </h2>
+        </div>
+    )
+}
+
+export default TitleBar;
