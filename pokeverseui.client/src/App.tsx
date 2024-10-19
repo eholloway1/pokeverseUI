c3e5f2bf7442c3d55f2b1629fdc27bf8e20af2c8
Evan Holloway <eholloway00@gmail.com>
2024-10-19 14:00:58 -0700

    not working

--- pokeverseui.client/src/App.tsx
+++ pokeverseui.client/src/App.tsx
@@ -1,23 +1,53 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import PokeCards from './poke-card.tsx';
-import {PokeJson, PokeResults} from './Models/poke-json.tsx';
+import { PokeJson, PokeResults } from './Models/poke-json.tsx';
+import { Container, Box, TextField } from '@mui/material';
+import TitleBar from "./title-bar.tsx";
+
+const url = "https://pokeapi.co/api/v2/pokemon?limit=151";
 
 function App() {
-  const url = "https://pokeapi.co/api/v2/pokemon?limit=151";
 
-  const [unfilteredPoke, setUnfilteredPoke] = useState<PokeJson[] | undefined>([]);
-  const [jsonResult, setJsonResult] = useState<PokeResults>();
+    const [unfilteredPoke, setUnfilteredPoke] = useState<PokeJson[] | undefined>([]);
+    const [filteredPoke, setFilteredPoke] = useState<PokeJson[] | undefined>([]);
+    const [jsonResult, setJsonResult] = useState<PokeResults>();
 
-  useEffect(() => {
-    fetch(url).then(response => response.json()).then(data => setJsonResult(data)).finally(() => setUnfilteredPoke(jsonResult?.results));
-  }, [unfilteredPoke]);
+    const fetcher = async () => {
+        const response = await fetch(url);
+        const json = await response.json();
+        setJsonResult(json);
+        setUnfilteredPoke(jsonResult?.results);
+        setFilteredPoke(unfilteredPoke)
+    }
 
-  return (
-    <div className="flexContainer">
-      {unfilteredPoke?.map((poke) =>  <PokeCards PokeObj={poke} />)}
-    </div>
-  );
+    useEffect(() => {
+        /*fetch(url).then(response => response.json())
+            .then(data => setJsonResult(data))
+            .finally(() => setUnfilteredPoke(jsonResult?.results));
+        setFilteredPoke(unfilteredPoke);*/
+        fetcher();
+    }, []); console.log(jsonResult?.results);
+
+    function handlePokeSearch(e) {
+        const value = e.target.value;
+        const regex = new RegExp(value, 'gi');
+        const filtered = unfilteredPoke?.filter((poke) => {
+            return poke.name.match(regex);
+        });
+        setFilteredPoke(filtered);
+    }
+
+    return (
+        <Container className="flexContainer">
+            <TitleBar />
+            <Box display='flex' p={3}>
+                <TextField id='outlined-basic' label='Pokemon Search...' variant='outlined'
+                    onChange={(e) => { handlePokeSearch(e); }} />
+            </Box>
+            {filteredPoke?.map((poke) => <PokeCards PokeObj={poke} />)}
+        </Container>
+    );
 }
 
 export default App;
--- pokeverseui.client/src/main.tsx
+++ pokeverseui.client/src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import TitleBar from "./title-bar.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TitleBar/>
     <App />
   </StrictMode>,
 )
