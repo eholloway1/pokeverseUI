0e1196839ffe8f0b9f7d541757b4ef1a516befd2
Evan Holloway <evan.holloway@alaskaair.com>
2025-02-22 07:53:19 -0800

    adds move to poke (#8)
    * adds move to poke
    
    * adds move to poke
    
    * removes css
    
    * adds full moves fixes css

--- pokeverseui.client/src/poke-card.tsx
+++ pokeverseui.client/src/poke-card.tsx
@@ -1,26 +1,32 @@
 import Card from 'react-bootstrap/Card';
 import { useState } from 'react';
-import {PokeJson, pokeDetails} from "./Models/poke-json.tsx";
+import {PokeJson, PokeDetails} from "./Models/poke-json.tsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 interface CardProps {
   PokeObj: PokeJson;
 }
 
 function PokeCards({ PokeObj }: CardProps) {
-    const [pokeDetails, setPokeDetails] = useState<pokeDetails>();
-    
+    const [pokeDetails, setPokeDetails] = useState<PokeDetails>();
+
     fetch(PokeObj.url).then(response => response.json()).then(data => setPokeDetails(data));
     
+    const move = pokeDetails?.moves.map(move => move.move.name);
+
     return (
-      <Card style={{width:'9rem'}}>
-        <Card.Img variant="top" src={pokeDetails?.sprites.front_default} />
+      <Card key={'Dark'} bg={'dark'} className="mb-2" text={'white'} >
+        <Card.Img variant="top" src={pokeDetails?.sprites.front_default} alt={PokeObj.name} style={{backgroundColor: "gray"}}/>
         <Card.Body>
             <Card.Title>
                 {PokeObj.name}
             </Card.Title>
+            <>
+              {move?.slice(0,4).map(m => <Card.Text style={{padding: 0, border: 0, margin: 0}}>{m}</Card.Text>)}
+            </>
         </Card.Body>
       </Card>
   );
 }
 
-export default PokeCards;   
+export default PokeCards;
